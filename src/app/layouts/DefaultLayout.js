d7222639151c93e6c822b23c5ba8b254d90347fd
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-10-12 13:25:37 +0200

    [ADD]: footer and meta data

---
+++ src/app/layouts/DefaultLayout.js
@@ -0,0 +1,37 @@
+'use client';
+import { useState, useEffect } from 'react';
+import MegaMenu from '../components/ui/aside/MegaMenu';
+import Main from '../components/ui/main/Main';
+import Header from '../components/ui/aside/Header';
+import Footer from '../components/ui/aside/Footer';
+
+export default function DefaultLayout({ children }) {
+    const [MenuOpen, setMenuOpen] = useState(false);
+    const [pageLoaded, setPageLoaded] = useState(false);
+    const [key, setKey] = useState(0);
+
+    const toggleMenu = () => {
+        setMenuOpen(!MenuOpen);
+    };
+
+    useEffect(() => {
+        setPageLoaded(true);
+    }, [key]);
+
+    useEffect(() => {
+        setKey(prevKey => prevKey + 1);
+    }, [children]);
+
+    const MenuContent = pageLoaded ? (
+        <MegaMenu toggleMenu={toggleMenu} isopen={MenuOpen ? 'open' : 'close'} />
+    ) : null;
+
+    return (
+        <>
+            <Header toggleMenu={toggleMenu} isopen={MenuOpen ? 'open' : 'close'} />
+            {MenuContent}
+            <Main>{children}</Main>
+            <Footer/>
+        </>
+    );
+}
