4ea0b1de7a0aa6728366b089c5123c245a7f07be
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-10-22 23:22:47 +0200

    [FIX]: use next_public env

--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -1,7 +1,6 @@
 'use client';
 
 import Button from '../components/ui/button/Button';
-import Text from '../components/ui/textual/Text';
 import Title from '../components/ui/textual/Title';
 import Hero from '../components/ui/wrapper/Hero';
 import DefaultLayout from '../layouts/DefaultLayout';
@@ -16,7 +15,6 @@ const Page = () => {
                 <Title>
                     About me
                 </Title>
-                <Text>test base url : {process.env.BASE_URL}</Text>
                 <Button className={"step-1"} variant={"primary"} href="/" transition>back to home</Button>
             </Hero>
         </MainContent>
