2edfb431648356f272d2d313bc8c6962209bc761
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-10-23 00:12:11 +0200

    [TEST]: disable cache for api v5

--- src/app/api/github/route.js
+++ src/app/api/github/route.js
@@ -1,18 +1,32 @@
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const githubUsername = 'domov44'; 
 
-  const response = await fetch(`https://api.github.com/users/${githubUsername}`, {
-    headers: {
-      Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`
+  try {
+    const response = await fetch(`https://api.github.com/users/${githubUsername}`, {
+      headers: {
+        Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`
+      }
+    });
+
+    if (!response.ok) {
+      console.error('GitHub API error:', await response.text());
+      return NextResponse.json(
+        { message: 'Erreur lors de la récupération des données GitHub' }, 
+        { status: response.status }
+      );
     }
-  });
 
-  if (!response.ok) {
-    return NextResponse.json({ message: 'Erreur lors de la récupération des données GitHub' }, { status: 500 });
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return NextResponse.json(
+      { message: 'Erreur serveur' }, 
+      { status: 500 }
+    );
   }
-
-  const data = await response.json();
-  return NextResponse.json(data);
 }
